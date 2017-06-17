9446db044b4fc2cdc737a38712a8e74e24e2df92
Jeffrey Rohlman <jeffreyrohlman@jeffreys-mbp.attlocal.net>
2017-06-16 17:01:04 -0700

    Created initial project

---
+++ Swift Language Basics/Swift Language Basics/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Swift Language Basics
+//
+//  Created by Jeffrey Rohlman on 6/16/17.
+//  Copyright © 2017 Jeffrey Rohlman. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
